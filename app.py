8453c405f8792caac26b4ab38dbe29de1d5bef35
PhoenixZero4 <Phoenixzero4@hotmail.com>
2018-11-18 16:04:08 -0800

    CSS stage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Markup, request, url_for, redirect, flash, session, logging, g
+from flask import Flask, render_template, Markup, request, url_for, redirect, flash, session, logging, g, abort
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from data import info
@@ -6,15 +6,19 @@ import sqlite3
 
 MyApp = Flask(__name__)
 MyApp.secret_key = 'leroybiggins'
+MyApp.debug = True
 info = info()
 
+class RegisterForm(Form):
+  username = StringField('Username',[validators.Length(min=1,max=100)])
+  email = StringField('Email',[validators.Length(min=5,max=100)])
+  password = PasswordField('Password',[validators.DataRequired(),validators.EqualTo('confirm',message='Passwords do not match')])
+  confirm = PasswordField('Confirm Password')   
 
-MyApp.debug = True
+class LoginForm(Form):
+  username = StringField('Username')
+  password = PasswordField('Password')
 
-@MyApp.route('/login')
-def login():
-  return render_template('login.html')
-  
 @MyApp.route('/')
 def home():
   return render_template('index.html')
@@ -26,18 +30,16 @@ def about():
 @MyApp.route('/info')
 @MyApp.route('/info/<dog>/')
 def dogInfo(dog=None):
-  return render_template('info.html', dog=dog) 
+  id=0
+  for n in info:
+    if dog == n['name']:
+      id = n['id']
+    var = info[id-1]
+  return render_template('info.html', dogname=dog, info=info, var=var) 
   
 @MyApp.route('/gallery')
 def pics():  
   return render_template('gallery.html', info = info)
-  
-
-class RegisterForm(Form):
-  username = StringField('Username',[validators.Length(min=1,max=100)])
-  email = StringField('Email',[validators.Length(min=5,max=100)])
-  password = PasswordField('Password',[validators.DataRequired(),validators.EqualTo('confirm',message='Passwords do not match')])
-  confirm = PasswordField('Confirm Password')    
 
 @MyApp.route('/register', methods= ['GET','POST'])
 def register():
@@ -56,13 +58,47 @@ def register():
       con.close()
       flash('Your account has been registered. You can now login','success')
       redirect(url_for('login'))
-      return render_template('register.html',form=form)
+      return render_template('login.html',form=form)
     except:
       con.close()
       flash('That username is already taken','error')
   return render_template('register.html',form=form)
 
-              
+@MyApp.route('/login', methods=['GET','POST'])
+def login():
+  form = LoginForm(request.form)
+
+  if request.method == 'POST':
+    user = request.form['username']
+    password_maybe = request.form['password']
+    password = None
+    con = sqlite3.connect('test.db')
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    result = cur.execute("SELECT * FROM users WHERE username = '"+ user+"'")
+    
+    for row in result:
+      session['id'] = row['email'] + row['username']
+      session['username'] = row['username']
+      password = row['password']
+    con.close()
+    if password_maybe == password:
+      return render_template('index.html',form=form)
+    else:
+      flash('Incorrect username or password','error')
+      return render_template('login.html',form=form)
+  return render_template('login.html',form=form)   
+
+@MyApp.route('/logout')
+def logout():
+  if 'username' in session:
+    session.pop('id',None)
+    flash('You have successfully logged out of your account','success')
+    return render_template('index.html')
+  return 'This shouldn\'t happen. \nYou are not logged in'
+
+
+  
 if __name__ == "__main__":
   MyApp.run()
  
--- data.py
+++ data.py
@@ -2,24 +2,48 @@ def info():
     dogs = [
         {
             'id' : 1,
+            'pics': {'Brodi'},
             'name' : 'Brodi',
             'breed' : 'Border Collie / Black Lab',
             'age' : '4',
-            'sex' : 'male'
+            'sex' : 'male',
+            'neutered': 'neutered',
+            'origin': 'Brodi came to Kelowna Collies from Creston in the Kootenay region of British Columbia',
+            'likes': 'Brodi lives to play fetch. Not food driven like a lot of dogs, he is crazy for games whether it\'s playing frisbee, chasing the ball, or swimming after a floating toy.',
+            'weight': '27 kg',
+            'color':'black with a white blaze on chest',
+            'dislikes':'He can be nervous around children.',
+            'remarks':'Brodi has a brother at Kelowna Collies, a Blue Heeler cross, Lael.'
         },
         {
             'id' : 2,
+            'pics':{'Lael','Lael1','Lael2','Lael3','Lael4'},
             'name' : 'Lael',
             'breed' : 'Blue Heeler (Australian Cattle Dog)',
-            'age' : 2,
+            'age' : '2',
             'sex' : 'male',
+            'neutered':'neutered',
+            'origin':'Lael was born on New Year\'s Day, 2016 in the Creson Valley, British Columbia',
+            'likes': 'Lael lives to herd other dogs. He has very strong working dog instincts.',
+            'weight': '23 kg ',
+            'color' : 'Tri-color',
+            'dislikes': 'Lael is sometimes standoffish with intact males',
+            'remarks' : 'Great with children and very smart.'
         },
         {
             'id' : 3,
+            'pics': {'Leroy'},
             'name' : 'Leroy',
             'breed' : 'Border Collie',
             'age' : '7',
-            'sex' : 'male' 
+            'sex' : 'male', 
+           'neutered':'not neutered',
+            'origin':'Leroy was abandoned in Kelowna.',
+            'likes': 'Leroy loves to play ball, frisbee and swim. He has a sweet demeanor and is great with children and other dogs.',
+            'weight': '25 kg',
+            'color' : 'Black',
+            'dislikes': 'Leroy is extremely good natured.',
+            'remarks' : 'Leroy has minor hip problems but you would never guess from his high energy and playful attitude.'
         }
     ]
 
